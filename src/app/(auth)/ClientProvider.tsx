fe32b40e7260b4b797de23cb121f8250867ad7b6
hisuzuya <hidehis111@gmail.com>
2025-04-20 18:19:12 +0900

    react-toastifyを追加し、APIトークンモーダルにトースト通知機能を実装しました。また、データベースマイグレーションを追加し、ToolGroupテーブルの構造を変更しました。bun.lockbファイルも更新しました。

--- src/app/(auth)/ClientProvider.tsx
+++ src/app/(auth)/ClientProvider.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { ToastContainer } from "react-toastify";
 import { TRPCReactProvider } from "@/trpc/react";
 import { SessionProvider } from "next-auth/react";
 
 export const ClientProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <TRPCReactProvider>
+      <ToastContainer />
       <SessionProvider>{children}</SessionProvider>
     </TRPCReactProvider>
   );
