8ed9895c85a75d1ef9411af9bb949d167b8863a6
Yuki992411 <yukikom@tech-neighbor.net>
2025-06-04 01:02:24 +0900

    [fix] add ThemeProvider

--- src/app/_components/ClientProvider.tsx
+++ src/app/_components/ClientProvider.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+import { Toaster } from "sonner";
 import { ToastContainer } from "react-toastify";
 import { TRPCReactProvider } from "@/trpc/react";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const ClientProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <TRPCReactProvider>
-      <ToastContainer />
-      <SessionProvider>{children}</SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ToastContainer />
+        <Toaster position="top-center" /> {/* For chat */}
+        <SessionProvider>{children}</SessionProvider>
+      </ThemeProvider>
     </TRPCReactProvider>
   );
 };
