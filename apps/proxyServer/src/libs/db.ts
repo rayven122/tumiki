023ce05fa31ed8f9f55f5455c9c206e5dc24f738
hisuzuya <hidehis111@gmail.com>
2025-06-07 09:45:35 +0900

    Refactor code structure for improved readability and maintainability

---
+++ apps/proxyServer/src/libs/db.ts
@@ -0,0 +1,41 @@
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+import { fieldEncryptionMiddleware } from "prisma-field-encryption";
+
+const createPrismaClient = () => {
+  // websocket を使った接続を使う
+  neonConfig.webSocketConstructor = ws;
+  const connectionString = `${process.env.DATABASE_URL}`;
+  const adapter = new PrismaNeon({ connectionString });
+
+  const client = new PrismaClient({
+    adapter,
+    log: ["query", "error", "warn"],
+
+    omit: {
+      userMcpServerConfig: {
+        envVars: true,
+      },
+    },
+  });
+
+  // HACK: fieldEncryptionExtension　が使えないため、fieldEncryptionMiddlewareを使う
+  client.$use(fieldEncryptionMiddleware());
+  // // フィールド暗号化のための拡張
+  // client.$extends(fieldEncryptionExtension());
+
+  return client;
+};
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+export type Db = typeof db;
---
+++ apps/proxyServer/src/types/config.ts
@@ -0,0 +1,22 @@
+export type TransportConfigStdio = {
+  type?: "stdio";
+  command: string;
+  args?: string[];
+  env?: Record<string, string>;
+};
+
+export type TransportConfigSSE = {
+  type: "sse";
+  url: string;
+};
+
+export type TransportConfig = TransportConfigSSE | TransportConfigStdio;
+export type ServerConfig = {
+  name: string;
+  toolNames: string[];
+  transport: TransportConfig;
+};
+
+export type Config = {
+  servers: ServerConfig[];
+};
