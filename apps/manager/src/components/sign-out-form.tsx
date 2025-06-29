22b215bbc1d3207016cd01198b28024c54f4a829
hisuzuya <hidehis111@gmail.com>
2025-06-29 10:11:25 +0900

    fix: SignOutFormのクラス名の順序を修正

--- apps/manager/src/components/sign-out-form.tsx
+++ apps/manager/src/components/sign-out-form.tsx
@@ -2,7 +2,7 @@ export const SignOutForm = () => {
   return (
     <a
       href="/auth/logout"
-      className="w-full px-1 py-0.5 text-left text-red-500 block"
+      className="block w-full px-1 py-0.5 text-left text-red-500"
     >
       Sign out
     </a>
