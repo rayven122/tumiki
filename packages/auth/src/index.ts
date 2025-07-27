40bfc153b932f2b4d47cfe875e1002987ce7920b
hisuzuya <hidehis111@gmail.com>
2025-07-27 11:35:59 +0900

    refactor: 不要なOAuthプロバイダー設定の削除とエラーログ出力の整理

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -6,31 +6,8 @@ import { ManagementClient } from "auth0";
 export type { SessionData } from "@auth0/nextjs-auth0/types";
 export * from "./oauth.js";
 export * from "./errors.js";
-
-// OAuth provider configurations
-export {
-  OAUTH_PROVIDERS,
-  PROVIDER_CONNECTIONS,
-  OAUTH_PROVIDER_CONFIG,
-} from "./providers/index.js";
-export type {
-  OAuthProvider,
-  OAuthScope,
-  OAuthProviderConfig,
-} from "./providers/index.js";
+export * from "./providers/index.js";
 export * from "./providers/validation.js";
-
-// export const auth0 = new Auth0Client({
-//   domain: 'your-tenant.auth0.com',
-//   clientId: 'YOUR_CLIENT_ID',
-//   appBaseUrl: 'http://localhost:3000',
-//   secret: '長くてランダムな文字列', // 32文字以上推奨
-//   clientAuthentication: {
-//     clientSecret: 'YOUR_CLIENT_SECRET'
-//     // または
-//     // clientAssertionSigningKey: '...'
-//   }
-// });
 
 export const auth0 = new Auth0Client();
 
