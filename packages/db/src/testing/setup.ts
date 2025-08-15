1404aeda8d0845409feaf102354577753fb38c4e
hisuzuya <hidehis111@gmail.com>
2025-08-15 20:23:48 +0900

    refactor: simplify data size formatting in LogsAnalyticsTab and RequestDataDetailModal

--- packages/db/src/testing/setup.ts
+++ packages/db/src/testing/setup.ts
@@ -2,9 +2,6 @@ import { vi } from "vitest";
 
 import { initialize } from "../../prisma/generated/fabbrica/index.js";
 
-// vPrismaクライアント（トランザクション対応）をそのまま使用
-// 拡張機能のテストは、後で適用されたクライアントを直接テストする
-
 vi.mock("../wsClient.js", () => ({
   db: vPrisma.client,
 }));
